754f78eead6bbb28572f9708757af6180f1dc646
Joe Streifel <josephstreifel@gmail.com>
2021-09-20 16:56:27 -0700

    updates am/pm time model to work correctly. cleans code, updates commetns.

--- app.js
+++ app.js
@@ -39,12 +39,6 @@ let parisShop = createShop(paris);
 let lima = ['Lima', 2, 16, 4.6];
 let limaShop = createShop(lima);
 
-//create function for calculating cookies sold per hour
-//6 AM to 8 PM (exclusive). Account for AM/PM. 6:00 to 20:00.
-//calculate total
-//return array (15 length)
-//each hour = [hour, AM/PM, cookies sold]
-
 //Caluclate cookies sold in 1 hour
 function hourCookies(locObj){
   let cookies = Math.floor(locObj.cookiePerSale * locObj.custPerHour());
@@ -55,17 +49,26 @@ function hourCookies(locObj){
 function dailySales (locObj){
   let daySales = []; //initialize empty array for storage of daily sales data
   let totalSales = 0; //initialize total sales for storage
-  for (let i = 6, j = 0; i < 20; i++, j++){ //for loop that steps through from open hour to close hour
+
+  //step through each hour (0600 to 2000) of operation. Generate sales data
+  for (let i = 6, j = 0; i < 20; i++, j++){
     let hourSales = [];
 
-    //log the time
-    if(i < 13){
+    //log the time - from 24 hour clock to 12 hour clock
+    if(i < 12 && i !== 0){
       hourSales[0] = i;
       hourSales[1] = 'am';
+    }else if(i == 12){
+      hourSales[0] = i;
+      hourSales[1] = 'pm';
+    }else if(i == 0){
+      hourSales[0] = 12;
+      hourSales[1] = 'am';
     }else{
-      hourSales[0] = i-12;
+      hourSales[0] = i - 12;
       hourSales[1] = 'pm';
     }
+
     //log cookies sold
     hourSales[2] = hourCookies(locObj);
     //Update total
@@ -95,25 +98,26 @@ function addData(locObj) {
 
   //Step through todaySales data to publish each hour to page
   for (let i = 0; i < locObj.todaySales.length; i++){
-    //create list item and decide what to fill it with.
     let hour = document.createElement('li');
+    //Indexes 0 through 'length - 2' contain hourly data
     if(i < (locObj.todaySales.length - 1)){
       let sales = hourlySales[i];
       let text = sales[0] + sales[1] + ': ' + sales[2] + ' cookies';
       hour.innerText = text;
-
-    }else if(i === (locObj.todaySales.length - 1)){ //final array item is total, change display of info
+    //Index 'length - 1' contains total cookies sold.
+    }else if(i === (locObj.todaySales.length - 1)){
       let totSales = hourlySales[i];
       let text = totSales[0] + totSales[1] + 'cookies';
       hour.innerText = text;
     }
     salesData.appendChild(hour);
   }
-  //Add ul with each hour and total underneath heading.
+  //Add <ul> with each hour and total underneath heading.
   container.appendChild(salesData);
 }
 
 // CREATE AND ADD SALES DATA FOR ALL SHOPS UPON LOAD
+
 dailySales(seattleShop);
 addData(seattleShop);
 
